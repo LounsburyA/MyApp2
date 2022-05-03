8e333660397445ae6831d9ea6fa878e263de29fe
Andrew Lounsbury <lounsburya@gmail.com>
2022-05-03 08:03:11 -0500

    trying to get save to work

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+// 
+Console.WriteLine("The current time is " + DateTime.Now);
+var name = "Andrew";
+Console.WriteLine("Hello" + name + "!");
+Console.WriteLine($"Hello {name}!");
+
+Console.WriteLine($"Hello {name.ToUpper()}!");
+
+int i = 10;
+var j = i + 1;
+
+Console.WriteLine(6 + 6);
+
+// string name ="Andrew";
+// Console.WriteLine(name);
+
+using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Andrew";
+            Console.WriteLine(name);
+        }
+    }
+}
